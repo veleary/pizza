fda56641096c9ebb76173e01dce86fffded703c5
veleary <veleary@gmail.com>
2013-09-11 10:49:45 -0500

    Add failure test method for vegetarian in the topping method

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -4,14 +4,18 @@ require 'rspec'
 require_relative '../pizza'
 
 describe Pizza::Pie do
+end
 
-end 
 
 describe Pizza::Topping do
   describe '.initialize' do
     it "sets the name of the topping" do
       topping = Pizza::Topping.new('olives')
       expect(topping.name).to eq('olives')
+    end
+  it "sets whether or not the topping is vegetarian" do
+  topping= Pizza::Topping.new('bell peppers',vegetarian = true)
+  expect(topping.vegetarian).to eq(true)
     end
   end
 end
