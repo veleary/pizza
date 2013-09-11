14837cd85b11d7aab55d41dbd55e6f5e574bd0c0
veleary <veleary@gmail.com>
2013-09-11 10:55:47 -0500

    Add vegetarian instance variable in initialize method

--- pizza.rb
+++ pizza.rb
@@ -4,14 +4,16 @@ module Pizza
   end
 
   class Topping
-    attr_accessor :name
+    attr_accessor :name, :vegetarian
  # Instantiate a new topping.
  #
- # name - The String name of the topping. 
- #
+ # name       - The String name of the topping. 
+ # vegetarian - The Boolean indicating whether or not the topping is vegetarian. 
+ # 
  # Returns a new Topping object. 
- def initialize(name)
-    @name = name
+ def initialize(name,vegetarian = false)
+    @name = name 
+    @vegetarian = vegetarian   
     end
   end
 end
