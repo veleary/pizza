678dd2bd6630c3456ce8ea0ccc93ea39cf6378cd
veleary <veleary@gmail.com>
2013-09-11 10:38:12 -0500

    Add initialize method for toppings

--- pizza.rb
+++ pizza.rb
@@ -4,6 +4,14 @@ module Pizza
   end
 
   class Topping
-
+    attr_accessor :name
+ # Instantiate a new topping.
+ #
+ # name - The String name of the topping. 
+ #
+ # Returns a new Topping object. 
+ def initialize(name)
+    @name = name
+    end
   end
 end
